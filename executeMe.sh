fe23f093553f34d47749d558594dc82ea64d046f
Qiyuan Pu <pqy7172@gmail.com>
2018-09-04 22:32:43 +0800

    update

--- executeMe.sh
+++ executeMe.sh
@@ -12,13 +12,13 @@ fi
 
 /usr/bin/ssh-add /home/pqy7172/.ssh/cs3
 
-scp puqiyuan@cs3.swfu.edu.cn:/home/staff/puqiyuan/shadowsocks.json ./shadowsocks.json
+scp puqiyuan@cs3.swfu.edu.cn:/home/staff/puqiyuan/shadowsocks.json ~/.shadowsocks.json
 
-scp puqiyuan@cs3.swfu.edu.cn:/home/staff/puqiyuan/Info.csv ./Info.csv
+scp puqiyuan@cs3.swfu.edu.cn:/home/staff/puqiyuan/Info.csv ~/.Info.csv
 
-scp puqiyuan@cs3.swfu.edu.cn:/home/staff/puqiyuan/Info.csv ./port
+scp puqiyuan@cs3.swfu.edu.cn:/home/staff/puqiyuan/Info.csv ~/.port
 
-sed -i "s/.*server.*/\"server\":\"$ip\",/g" shadowsocks.json
+sed -i "s/.*server.*/\"server\":\"$ip\",/g" ~/.shadowsocks.json
 
 scp ~/.shadowsocks.json  root@$ip:/etc/shadowsocks.json
 
